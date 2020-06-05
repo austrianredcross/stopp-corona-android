8d8eb96c82e89c93ad847dc53be2459d1fa1641f
Justus Klawisch <justusklawisch@web.de>
2020-06-05 12:22:11 +0200

    CTAA-1436: Added bottomSheet basics

---
+++ app/src/main/java/at/roteskreuz/stopcorona/screens/dashboard/changelog/ChangelogFragment.kt
@@ -0,0 +1,8 @@
+package at.roteskreuz.stopcorona.screens.dashboard.changelog
+
+import at.roteskreuz.stopcorona.R
+import at.roteskreuz.stopcorona.skeleton.core.screens.base.fragment.BaseFragment
+
+class ChangelogFragment : BaseFragment(R.layout.fragment_changelog) {
+
+}
