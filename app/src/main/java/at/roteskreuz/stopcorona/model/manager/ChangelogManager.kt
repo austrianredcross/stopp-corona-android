14df73a359d46cdec4c859551e87c648b395492a
Justus Klawisch <justusklawisch@web.de>
2020-06-05 17:17:22 +0200

    CTAA-1436: Validation of already displayed changelogs

--- app/src/main/java/at/roteskreuz/stopcorona/model/manager/ChangelogManager.kt
+++ app/src/main/java/at/roteskreuz/stopcorona/model/manager/ChangelogManager.kt
@@ -3,6 +3,7 @@ package at.roteskreuz.stopcorona.model.manager
 import android.content.SharedPreferences
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
+import at.roteskreuz.stopcorona.R
 import at.roteskreuz.stopcorona.constants.Constants.Prefs.CHANGELOG_MANAGER_PREFIX
 import at.roteskreuz.stopcorona.skeleton.core.utils.intSharedPreferencesProperty
 
@@ -28,16 +29,41 @@ class ChangelogManagerImpl(
         private const val PREF_LAST_SEEN_CHANGELOG_ID = CHANGELOG_MANAGER_PREFIX + "last_seen_changelog_id"
     }
 
+    /**
+     * Changelog content for a list of specific versions.
+     *
+     * [Changelog.id] has to be increased for every new changelog.
+     * [Changelog.versions] contains the list of versions this changelog applies to.
+     */
+    private val changelog = Changelog(
+        id = 1,
+        versions = listOf("2.0.0", "2.0.1"),
+        title = R.string.changelog_title_v2_0_0,
+        description = R.string.changelog_description_v2_0_0,
+        callToAction = R.string.changelog_cta_v2_0_0,
+        image = R.drawable.handshake_deactive
+    )
+
     private var lastSeenChangelogId: Int by preferences.intSharedPreferencesProperty(PREF_LAST_SEEN_CHANGELOG_ID, 0)
 
-    override fun getChangelogForVersion(version: String): Changelog {
-        TODO("Not yet implemented")
+    private val hasBeenDisplayed: Boolean
+        get() = changelog.id <= lastSeenChangelogId
+
+    override fun getChangelogForVersion(version: String): Changelog? {
+        return if (changelog.versions.contains(version) && hasBeenDisplayed.not()) {
+            lastSeenChangelogId = changelog.id
+            changelog
+        } else {
+            null
+        }
     }
 }
 
 data class Changelog(
     val id: Int,
+    val versions: List<String>,
     @StringRes val title: Int,
-    @DrawableRes val image: Int,
-    @StringRes val description: Int
+    @StringRes val description: Int,
+    @StringRes val callToAction: Int,
+    @DrawableRes val image: Int
 )
