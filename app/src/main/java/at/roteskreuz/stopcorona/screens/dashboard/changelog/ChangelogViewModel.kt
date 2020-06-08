a705809cc3937ec412d161eb4b07718e1d7809a3
Justus Klawisch <justusklawisch@web.de>
2020-06-08 12:58:54 +0200

    CTAA-1436: Added changelog ui basics

--- app/src/main/java/at/roteskreuz/stopcorona/screens/dashboard/changelog/ChangelogViewModel.kt
+++ app/src/main/java/at/roteskreuz/stopcorona/screens/dashboard/changelog/ChangelogViewModel.kt
@@ -1,4 +1,13 @@
 package at.roteskreuz.stopcorona.screens.dashboard.changelog
 
-class ChangelogViewModel {
+import at.roteskreuz.stopcorona.model.manager.ChangelogManager
+import at.roteskreuz.stopcorona.skeleton.core.model.helpers.AppDispatchers
+import at.roteskreuz.stopcorona.skeleton.core.screens.base.viewmodel.ScopedViewModel
+
+class ChangelogViewModel(
+    appDispatchers: AppDispatchers,
+    private val changelogManager: ChangelogManager
+) : ScopedViewModel(appDispatchers) {
+
+    fun getChangelogForVersion(version: String) = changelogManager.getChangelogForVersion(version)
 }
