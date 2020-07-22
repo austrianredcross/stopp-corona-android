4690d2fb2470395c9b00739c9c1e0050f3c2c2d7
Uli Luckas <uli.luckas@sinnerschrader.com>
2020-07-22 17:06:06 +0200

    CTAA-1120: Cache shared preferences observers

--- app/src/main/java/at/roteskreuz/stopcorona/model/repositories/QuarantineRepository.kt
+++ app/src/main/java/at/roteskreuz/stopcorona/model/repositories/QuarantineRepository.kt
@@ -172,12 +172,13 @@ class QuarantineRepositoryImpl(
     override var dateOfFirstMedicalConfirmation: ZonedDateTime? by preferences.nullableZonedDateTimeSharedPreferencesProperty(
         PREF_DATE_OF_FIRST_MEDICAL_CONFIRMATION)
 
-    private val dateOfFirstMedicalConfirmationObservable = preferences.observeNullableZonedDateTime(PREF_DATE_OF_FIRST_MEDICAL_CONFIRMATION)
+    private val dateOfFirstMedicalConfirmationObservable =
+        preferences.observeNullableZonedDateTime(PREF_DATE_OF_FIRST_MEDICAL_CONFIRMATION).shareReplayLast()
 
     private var dateOfFirstSelfDiagnose: ZonedDateTime? by preferences.nullableZonedDateTimeSharedPreferencesProperty(
         PREF_DATE_OF_FIRST_SELF_DIAGNOSE)
 
-    private var dateOfFirstSelfDiagnoseObservable = preferences.observeNullableZonedDateTime(PREF_DATE_OF_FIRST_SELF_DIAGNOSE)
+    private var dateOfFirstSelfDiagnoseObservable = preferences.observeNullableZonedDateTime(PREF_DATE_OF_FIRST_SELF_DIAGNOSE).shareReplayLast()
 
     private var dateOfFirstSelfDiagnoseBackup: ZonedDateTime?
         by preferences.nullableZonedDateTimeSharedPreferencesProperty(PREF_DATE_OF_FIRST_SELF_DIAGNOSE_BACKUP)
@@ -185,7 +186,7 @@ class QuarantineRepositoryImpl(
     private var dateOfLastSelfDiagnose: ZonedDateTime?
         by preferences.nullableZonedDateTimeSharedPreferencesProperty(PREF_DATE_OF_LAST_SELF_DIAGNOSE)
 
-    private val dateOfLastSelfDiagnoseObservable = preferences.observeNullableZonedDateTime(PREF_DATE_OF_LAST_SELF_DIAGNOSE)
+    private val dateOfLastSelfDiagnoseObservable = preferences.observeNullableZonedDateTime(PREF_DATE_OF_LAST_SELF_DIAGNOSE).shareReplayLast()
 
     private var dateOfLastSelfDiagnoseBackup: ZonedDateTime?
         by preferences.nullableZonedDateTimeSharedPreferencesProperty(PREF_DATE_OF_LAST_SELF_DIAGNOSE_BACKUP)
@@ -195,31 +196,32 @@ class QuarantineRepositoryImpl(
             PREF_DATE_OF_LAST_RED_CONTACT
         )
 
-    private val dateOfLastRedContactObservable = preferences.observeNullableInstant(PREF_DATE_OF_LAST_RED_CONTACT)
+    private val dateOfLastRedContactObservable = preferences.observeNullableInstant(PREF_DATE_OF_LAST_RED_CONTACT).shareReplayLast()
 
     private var dateOfLastYellowContact: Instant?
         by preferences.nullableInstantSharedPreferencesProperty(
             PREF_DATE_OF_LAST_YELLOW_CONTACT
         )
 
-    private val dateOfLastYellowContactObservable = preferences.observeNullableInstant(PREF_DATE_OF_LAST_YELLOW_CONTACT)
+    private val dateOfLastYellowContactObservable = preferences.observeNullableInstant(PREF_DATE_OF_LAST_YELLOW_CONTACT).shareReplayLast()
 
     private var dateOfLastSelfMonitoringInstruction: ZonedDateTime?
         by preferences.nullableZonedDateTimeSharedPreferencesProperty(
             PREF_DATE_OF_LAST_SELF_MONITORING
         )
 
-    private val dateOfLastSelfMonitoringInstructionObservable = preferences.observeNullableZonedDateTime(PREF_DATE_OF_LAST_SELF_MONITORING)
+    private val dateOfLastSelfMonitoringInstructionObservable =
+        preferences.observeNullableZonedDateTime(PREF_DATE_OF_LAST_SELF_MONITORING).shareReplayLast()
 
     private var showQuarantineEnd: Boolean
         by preferences.booleanSharedPreferencesProperty(PREF_SHOW_QUARANTINE_END, false)
 
-    private val showQuarantineEndObservable = preferences.observeBoolean(PREF_SHOW_QUARANTINE_END, false)
+    private val showQuarantineEndObservable = preferences.observeBoolean(PREF_SHOW_QUARANTINE_END, false).shareReplayLast()
 
     private var missingKeysUploaded: Boolean
         by preferences.booleanSharedPreferencesProperty(PREF_MISSING_KEYS_UPLOADED, false)
 
-    private val missingKeysUploadedObservable = preferences.observeBoolean(PREF_MISSING_KEYS_UPLOADED, false)
+    private val missingKeysUploadedObservable = preferences.observeBoolean(PREF_MISSING_KEYS_UPLOADED, false).shareReplayLast()
 
     override val hasSelfDiagnoseBackup: Boolean
         get() = dateOfFirstSelfDiagnoseBackup != null && dateOfLastSelfDiagnoseBackup != null
