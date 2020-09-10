918636c1c22ce920748aeb243e8f597bc91823ef
Jakob Haider <jakob.haider@bluesource.at>
2020-09-10 12:14:12 +0200

    add Koin to gms module and provide koin-module that provides common-exposure-client (that is based on google nearby)

---
+++ gms/src/main/java/at/roteskreuz/stopcorona/gms/di/ExposureModule.kt
@@ -0,0 +1,20 @@
+package at.roteskreuz.stopcorona.gms.di
+
+import at.roteskreuz.stopcorona.commonexposure.CommonExposureClient
+import at.roteskreuz.stoppcorona.google.GoogleExposureClient
+import com.google.android.gms.common.GoogleApiAvailability
+import com.google.android.gms.nearby.Nearby
+import org.koin.android.ext.koin.androidApplication
+import org.koin.dsl.module
+
+
+val exposureModule = module {
+
+    single { GoogleApiAvailability.getInstance() }
+    single { Nearby.getExposureNotificationClient(androidApplication()) }
+
+    single<CommonExposureClient> {
+        GoogleExposureClient(androidApplication(), get(), get())
+    }
+
+}
