11a8fd22ea1e04e7244ca79fcdcd5535f94f7d39
minkiapps <minkiapps@gmail.com>
2020-12-02 17:20:19 +0100

    Clamp CS exposure values to GMS allowed value range

--- app/src/huawei/java/at/roteskreuz/stopcorona/extensions/HmsToGmsMappings.kt
+++ app/src/huawei/java/at/roteskreuz/stopcorona/extensions/HmsToGmsMappings.kt
@@ -12,13 +12,18 @@ import com.huawei.hms.contactshield.PeriodicKey
 import org.threeten.bp.Instant
 import org.threeten.bp.temporal.ChronoUnit
 
+private const val MAXIMUM_RISK_SCORE = 4096
+private const val MAXIMUM_TRANSMISSION_LEVEL = 8
+private const val MAXIMUM_ATTENUATION_VALUE = 255
+private const val MAXIMUM_DURATION_MINUTES = 30
+
 fun PeriodicKey.toTemporaryExposureKey(): TemporaryExposureKey {
 
     return TemporaryExposureKey.TemporaryExposureKeyBuilder()
         .setKeyData(content)
         .setRollingStartIntervalNumber(periodicKeyValidTime.toInt())
         .setRollingPeriod(periodicKeyLifeTime.toInt())
-        .setTransmissionRiskLevel(initialRiskLevel)
+        .setTransmissionRiskLevel(initialRiskLevel.coerceIn(0, MAXIMUM_TRANSMISSION_LEVEL))
         .setReportType(reportType)
         .build()
 }
@@ -27,7 +32,7 @@ fun ContactSketch.toExposureSummary(): ExposureSummary {
     return ExposureSummary.ExposureSummaryBuilder()
         .setDaysSinceLastExposure(daysSinceLastHit)
         .setMatchedKeyCount(numberOfHits)
-        .setMaximumRiskScore(maxRiskValue)
+        .setMaximumRiskScore(maxRiskValue.coerceIn(0,MAXIMUM_RISK_SCORE))
         .setSummationRiskScore(summationRiskValue)
         .setAttenuationDurations(attenuationDurations)
         .build();
@@ -37,12 +42,21 @@ fun ContactSketch.toExposureSummary(): ExposureSummary {
 fun ContactDetail.toExposureInformation(): ExposureInformation {
     return ExposureInformationBuilder()
         .setDateMillisSinceEpoch(Instant.EPOCH.plus(dayNumber, ChronoUnit.DAYS).toEpochMilli())
-        .setAttenuationValue(attenuationRiskValue)
-        .setTransmissionRiskLevel(initialRiskLevel)
-        .setDurationMinutes(durationMinutes)
+        .setAttenuationValue(attenuationRiskValue.coerceIn(0, MAXIMUM_ATTENUATION_VALUE))
+        .setTransmissionRiskLevel(initialRiskLevel.coerceIn(0, MAXIMUM_TRANSMISSION_LEVEL))
+        .setDurationMinutes(clampTo5Granularity(durationMinutes).coerceIn(0, MAXIMUM_DURATION_MINUTES))
         .setAttenuationDurations(attenuationDurations)
-        .setTotalRiskScore(totalRiskValue)
+        .setTotalRiskScore(totalRiskValue.coerceIn(0, MAXIMUM_RISK_SCORE))
         .build()
+}
+
+private fun clampTo5Granularity(value : Int) : Int {
+    val rest = value % 5
+    return if(rest != 0) {
+        value - rest + 5
+    } else {
+        value
+    }
 }
 
 fun ExposureConfiguration.toDiagnosisConfiguration(): DiagnosisConfiguration {
